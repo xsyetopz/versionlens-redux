9aa2811dcbc3ad5f86a1e8149ac2ddcffab2dbd5
pflannery <61407-pflannery@users.noreply.gitlab.com>
2024-11-11 17:44:25 +0000

    Moved promiseSpawn process tests into domain

--- test/unit/infrastructure/all.tests.ts
+++ test/unit/infrastructure/all.tests.ts
@@ -2,5 +2,4 @@ export * as DotNetProvider from '#test/unit/providers/dotnet';
 export * as MavenProvider from '#test/unit/providers/maven';
 export * as NpmProvider from '#test/unit/providers/npm';
 export * as Parser from './parsers/all.tests';
-export * as Process from './process/promiseSpawnClient.tests';
 export * as Watcher from './watcher/packageFileWatcher.tests';
