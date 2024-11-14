3bf0231a3f8ff5f77149aeed21daa901d71e45b1
pflannery <61407-pflannery@users.noreply.gitlab.com>
2024-11-14 00:29:50 +0000

    Moved go defintions into go/defintions.ts

--- src/domain/providers/golang/definitions.ts
+++ src/domain/providers/golang/definitions.ts
@@ -2,16 +2,19 @@ import { CachingOptions } from '#domain/caching';
 import { HttpOptions, IHttpClient } from '#domain/clients';
 import { GoClient, GoConfig } from '#domain/providers/golang';
 
+export enum GoFeatures {
+  Caching = 'golang.caching',
+  Http = 'golang.http',
+  ApiUrl = 'golang.apiUrl',
+  FilePatterns = 'golang.files',
+  OnSaveChangesTask = 'golang.onSaveChanges',
+  PrereleaseTagFilter = 'golang.prereleaseTagFilter',
+}
+
 export interface IGoService {
-
   goCachingOpts: CachingOptions;
-
   goHttpOpts: HttpOptions;
-
   goConfig: GoConfig;
-
   goHttpClient: IHttpClient;
-
   goClient: GoClient;
-
 }
--- src/domain/providers/golang/definitions/eGoFeatures.ts
+++
@@ -1,8 +0,0 @@
-export enum GoFeatures {
-  Caching = 'golang.caching',
-  Http = 'golang.http',
-  ApiUrl = 'golang.apiUrl',
-  FilePatterns = 'golang.files',
-  OnSaveChangesTask = 'golang.onSaveChanges',
-  PrereleaseTagFilter = 'golang.prereleaseTagFilter',
-}
