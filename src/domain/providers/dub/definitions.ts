6e510554b2e73d44cc2aae194fa757ca7a1716f1
pflannery <61407-pflannery@users.noreply.gitlab.com>
2024-11-14 00:24:32 +0000

    Moved dub defintions into dub/defintions.ts

--- src/domain/providers/dub/definitions.ts
+++ src/domain/providers/dub/definitions.ts
@@ -2,16 +2,20 @@ import { ICachingOptions } from '#domain/caching';
 import { IHttpOptions, IJsonHttpClient } from '#domain/clients';
 import { DubClient, DubConfig } from '#domain/providers/dub';
 
+export enum DubFeatures {
+  Caching = 'dub.caching',
+  Http = 'dub.http',
+  DependencyProperties = 'dub.dependencyProperties',
+  ApiUrl = 'dub.apiUrl',
+  FilePatterns = 'dub.files',
+  OnSaveChangesTask = 'dub.onSaveChanges',
+  prereleaseTagFilter = 'dub.prereleaseTagFilter',
+}
+
 export interface IDubServices {
-
   dubCachingOpts: ICachingOptions;
-
   dubHttpOpts: IHttpOptions;
-
   dubConfig: DubConfig;
-
   dubJsonClient: IJsonHttpClient;
-
   dubClient: DubClient;
-
 }
--- src/domain/providers/dub/definitions/eDubFeatures.ts
+++
@@ -1,9 +0,0 @@
-export enum DubFeatures {
-  Caching = 'dub.caching',
-  Http = 'dub.http',
-  DependencyProperties = 'dub.dependencyProperties',
-  ApiUrl = 'dub.apiUrl',
-  FilePatterns = 'dub.files',
-  OnSaveChangesTask = 'dub.onSaveChanges',
-  prereleaseTagFilter = 'dub.prereleaseTagFilter',
-}
