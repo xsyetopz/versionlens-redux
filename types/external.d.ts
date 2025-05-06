70c6cff261195900588e13f08d7ea251c01fa17a
pflannery <61407-pflannery@users.noreply.gitlab.com>
2025-05-06 21:06:45 +0100

    added @npmcli/config and npm-registry-fetch to external types def

--- types/external.d.ts
+++ types/external.d.ts
@@ -1,2 +1,5 @@
 declare module "@npmcli/promise-spawn";
+declare module "@npmcli/config";
+declare module "@npmcli/config/lib/definitions";
+declare module "npm-registry-fetch";
 declare module "semver";
